59280d66e476fa434ad6fd300d458da4446bc8b1
Antoine Lehurt <keuwah@gmail.com>
2014-02-07 15:40:15 +0100

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,49 @@
+
+# dom-center
+
+  Centre a DOM element relative to another container
+
+## Installation
+
+  Install with [component(1)](http://component.io):
+
+    $ component install nk-components/dom-center
+
+## API
+
+### center(target, container, renderer)
+
+* target: Element that will be centred.
+* container: [optional] By default it uses `window` as container
+* renderer: [optional]
+  * `undefined`: centre the target using `transform` property
+  * `false`: the fn returns the position `{ x: Number, y: Number }`
+  * `Function`: the fn calls the custom renderer. `renderer(target, position)`
+
+### center.x(target, container, renderer)
+
+### center.y(target, container, renderer)
+
+## License
+
+  The MIT License (MIT)
+
+  Copyright (c) 2014 <copyright holders>
+
+  Permission is hereby granted, free of charge, to any person obtaining a copy
+  of this software and associated documentation files (the "Software"), to deal
+  in the Software without restriction, including without limitation the rights
+  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+  copies of the Software, and to permit persons to whom the Software is
+  furnished to do so, subject to the following conditions:
+
+  The above copyright notice and this permission notice shall be included in
+  all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+  THE SOFTWARE.
---
+++ index.js
@@ -0,0 +1,87 @@
+'use strict';
+
+var prefix = require('prefix');
+var transform = prefix('transform');
+
+
+exports = module.exports = center;
+
+function center(target, container, renderer) {
+  var pos = {
+    x: centerX(target, container, false),
+    y: centerY(target, container, false)
+  };
+
+  if (renderer === false) {
+    return pos;
+  }
+
+  if (typeof renderer === 'function') {
+    renderer(target, pos);
+    return;
+  }
+
+  target.style[transform] = 'translateX(' + pos.x + 'px) translateY(' + pos.y + 'px)';
+}
+
+
+exports.x = centerX;
+
+function centerX(target, container, renderer) {
+  // in case `translateX` has already been defined
+  target.style[transform] = 'translateX(0)';
+
+  var x = target.offsetLeft;
+  var targetW = target.clientWidth;
+  var containerW = (container) ? container.clientWidth : window.innerWidth;
+
+  var transX = ((containerW - targetW) >> 1) - x;
+
+  if (renderer === false) {
+    return transX;
+  }
+
+  if (typeof renderer === 'function') {
+    renderer(target, {
+      x: transX
+    });
+    return;
+  }
+
+  target.style[transform] = 'translateX(' + transX + 'px)';
+}
+
+
+exports.y = centerY;
+
+function centerY(target, container, renderer) {
+  // in case `translateY` has already been defined
+  target.style[transform] = 'translateY(0)';
+
+  var targetH = target.clientHeight;
+  var containerH;
+  var y;
+
+  if (container) {
+    containerH = container.clientHeight;
+    y = target.offsetTop;
+  } else {
+    containerH = window.innerHeight;
+    y = target.getBoundingClientRect().top;
+  }
+
+  var transY = ((containerH - targetH) >> 1) - y;
+
+  if (renderer === false) {
+    return transY;
+  }
+
+  if (typeof renderer === 'function') {
+    renderer(target, {
+      y: transY
+    });
+    return;
+  }
+
+  target.style[transform] = 'translateY(' + transY + 'px)';
+}
